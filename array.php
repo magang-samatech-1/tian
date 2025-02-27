f216c8832af9c3973ec1a520f62acf0570b29560
kristianusss <cinemaxgems@gmail.com>
2025-02-27 15:40:28 -0800

    perkenalan array

---
+++ array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<body>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> Array PHP</h1>
+
+ <?php
+
+//tipe data Array
+$kotak = array('anjing','kurakura','koala');
+$nama = ['Dede','Jamal','Kalpin'];
+
+//print_r($kotak);
+print_r($kotak);
+echo $nama [0];
+
+?>
+
+</body>
+</html>
