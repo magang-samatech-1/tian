e13dd425ad539b6bae083724e529bc80563618f9
kristianusss <cinemaxgems@gmail.com>
2025-02-27 15:12:03 -0800

    Belajar Php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
 <body>
-    <?php echo "<h1>Belajar Koding"; ?>
-        hallo saya belajar php
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+      halo semuaaaa!
+ <?php echo " Sekolah Koding"; ?>
+
 </body>
 </html>
