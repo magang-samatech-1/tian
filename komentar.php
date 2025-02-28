9f7e63fe726029a56f0126272814b1e4436e45de
kristianusss <cinemaxgems@gmail.com>
2025-02-27 16:32:05 -0800

    komentar 3 variabel

---
+++ komentar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+<body>
+
+ <?php 
+
+
+  $nama = "Sekolah koding 123";
+  $_123 = "text baru";
+
+  echo "<h1>$_123</h1>";
+  echo "Halo ". $nama ."<br>";
+  echo " Member";
+  
+  ?>
+
+  </body>
+</html>
