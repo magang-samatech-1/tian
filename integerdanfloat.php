7cd4eef9680d4d97b85213be135ef5720427a3ac
kristianusss <cinemaxgems@gmail.com>
2025-02-28 11:44:33 -0800

    Data integer dan float

---
+++ integerdanfloat.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+<body>
+
+ <?php 
+
+
+  $nama = "";
+  $nama2 = "float";
+
+  echo " Integer dan float  $nama <br>";
+  
+  $angka = 1000;
+  $angka2 = 100.123;
+
+  echo $angka + $angka2;
+  
+  ?>
+
+  </body>
+</html>
