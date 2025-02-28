a3c5eca0cfa8d5c9c9eca71d67dac6634aed4410
kristianusss <cinemaxgems@gmail.com>
2025-02-28 15:32:05 -0800

    FOR LOOP

---
+++ pengulangan.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<body>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> loop PHP</h1>
+
+ <?php
+
+for($i=0; $i<5; $i++)
+{
+    echo "---------";
+    echo "KUCING $i";
+    echo "--------- <br>";
+
+}
+
+?>
+
+</body>
+</html>
