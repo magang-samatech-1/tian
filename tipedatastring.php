794cc8e008e87f3d3ac6369a2c1216684219617e
kristianusss <cinemaxgems@gmail.com>
2025-02-28 11:35:01 -0800

    Tipe data string

---
+++ tipedatastring.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+<body>
+
+ <?php 
+
+
+  $nama = "Sekolah koding";
+  $nama2 = "Bermain koding";
+
+  echo "Selamat datang du \$nama>";
+  ?>
+
+  </body>
+</html>
