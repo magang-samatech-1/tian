f93d245d466e17a24ac283ca622e9c8a016f591e
kristianusss <cinemaxgems@gmail.com>
2025-02-28 11:53:30 -0800

    Oprator Matematika

---
+++ opmatematika.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+<body>
+
+ <?php 
+
+
+  $nama = "";
+  
+
+  echo "Oprator Matematika $nama <br> <br>";
+$angka = 1000;
+$angka2 = 3;
+$angka3 = 2;
+
+$angka *= $angka2;
+
+echo $angka;
+
+
+  ?>
+
+  </body>
+</html>
