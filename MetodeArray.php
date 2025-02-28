0ad783cf7841c1e8388f3e94439d346865485239
kristianusss <cinemaxgems@gmail.com>
2025-02-28 13:33:21 -0800

    Metode Array

---
+++ MetodeArray.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<body>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> Array PHP</h1>
+
+ <?php
+
+//tipe data Array
+$angka = [5, 10,2, 1, 6];
+$kotak = array('anjing','kurakura','koala','banteng');
+$nama = ['Dede','Jamal','Kalpin'];
+
+sort($angka);
+print_r($angka);
+
+?>
+
+</body>
+</html>
