575f38e99a9358ba6fd3a6d44fdcda0d8f64d030
kristianusss <cinemaxgems@gmail.com>
2025-02-28 14:11:27 -0800

    Associative Arrry

---
+++ AssociativeArray.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<body>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> Associative PHP</h1>
+
+ <?php
+
+$data = array ("nama " => "tian",
+               "umur"  => 22 ,  
+               "kerja" => "pengangguran"
+);
+
+$data['nama'] = "kristianus catur kadiek";
+    
+echo "Nama adalah saya " . $data['nama'];
+
+
+?>
+
+</body>
+</html>
