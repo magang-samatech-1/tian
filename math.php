7ac16c897cef62e64b48d50d9af85c9cce0dc077
kristianusss <cinemaxgems@gmail.com>
2025-02-28 12:03:01 -0800

    7 Metode math

---
+++ math.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+<body>
+
+ <?php 
+
+
+  $nama = "";
+  
+
+  echo "Metode Math $nama  <br>";
+$angka = 1000;
+$angka2 = 3;
+$angka3 = 2.64;
+
+$angka *= $angka2;
+
+echo "angka hari ini adalah ". max( 5, 10, 8, 5);
+
+
+  ?>
+
+  </body>
+</html>
