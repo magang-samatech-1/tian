a7149fe42bb5a06e6f238fbaf12542f2e65ce3cc
kristianusss <cinemaxgems@gmail.com>
2025-02-28 16:01:04 -0800

    foreach untuk asssosiatif array

---
+++ pengulangan3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<body>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> loop PHP</h1>
+
+ <?php
+
+$data = ['nama' => 'tian',
+         'umur' => 22,
+         'sifat' => 'rajin menabung']; 
+
+foreach ($data as $key => $value){
+    echo $value . "<br>";
+}
+
+?>
+
+</body>
+</html>
