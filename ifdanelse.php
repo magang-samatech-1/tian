caca219691f62caee6d08289dd55a54850209905
kristianusss <cinemaxgems@gmail.com>
2025-03-05 18:32:19 -0800

    If dan Else

---
+++ ifdanelse.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> logika di PHP</h1>
+
+ <?php
+
+$password = '1234';
+$password2 = '1234';
+
+if($password == $password2) {
+    echo 'anda berhasil masuk';
+} else {
+    echo 'gagal! passwordnya salah';
+}
+?>
+
+<body>
+</html>
