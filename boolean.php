69bc9bbf0f19396ea15ecebcae366a06f61ee740
kristianusss <cinemaxgems@gmail.com>
2025-03-05 16:54:10 -0800

    Tipe data boolean

---
+++ boolean.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> logika di PHP</h1>
+
+ <?php
+
+$hasil  = true;
+$hasil2 = false;
+
+?>
+
+<body>
+</html>
