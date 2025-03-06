d620c0c39eed56764fdd2271716266cf6a4cf4e9
kristianusss <cinemaxgems@gmail.com>
2025-03-06 13:55:29 -0800

    switch case

---
+++ switch.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> logika di PHP</h1>
+
+ <?php
+
+$nama = "Tian123n";
+
+switch ($nama) {
+    case 'triguna';
+    echo 'namanya triguna';
+    break;
+    case 'kalpin';
+    echo 'namanya kalpin';
+    break;
+    case 'sulaiman';
+    echo 'namanya sulaiman';
+    break;
+    default;
+    echo 'tidak ada yang benar';
+}
+?>
+
+<body>
+
+</html>
