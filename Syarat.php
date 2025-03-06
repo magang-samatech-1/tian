921858909df87084a428652a0de72eb135263591
kristianusss <cinemaxgems@gmail.com>
2025-03-06 12:20:14 -0800

    Lebih dari satu syarat

---
+++ Syarat.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> logika di PHP</h1>
+
+ <?php
+
+$uang_programmer = '1000';
+$keyboard = '2000';
+$uang_desainer = '3000';
+
+if($uang_programmer == $keyboard) {
+    echo 'dibeli!';
+} else if($uang_programmer == $keyboard) {
+    echo 'dibeli';
+}
+else{
+    echo 'ngga cukup';
+}
+?>
+
+<body>
+</html>
