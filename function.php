1b70924988161c08dcfff3ee404805c299d760bf
kristianusss <cinemaxgems@gmail.com>
2025-03-06 14:08:47 -0800

    function

---
+++ function.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> Fungsi PHP</h1>
+
+ <?php
+
+function panggil()
+{
+    echo "---------";
+    echo "Bisnis Tian";
+    echo "---------";
+}
+function jarak(){
+echo "<br>";
+}
+{
+panggil();
+jarak();
+panggil();
+jarak();
+
+}
+?>
+
+<body>
+
+</html>
