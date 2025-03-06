5728bc05e9ba80c6ca238ab3a716f5b6e975f3ab
kristianusss <cinemaxgems@gmail.com>
2025-03-06 13:40:14 -0800

    dan juga atau

---
+++ danjugaatau.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> logika di PHP</h1>
+
+ <?php
+
+$uang_programmer = '4000';
+$keyboard = '2000';
+$uang_desainer = '4000';
+
+if($uang_programmer > $keyboard
+    && $uang_programmer > $keyboard){
+
+    echo 'boleh beli keybboard';
+
+} else{ 
+    echo 'engga bisa dibeli';
+}
+?>
+
+<body>
+
+</html>
