5e341bfc84e208531c7f3b21ea244f4070edd66a
kristianusss <cinemaxgems@gmail.com>
2025-03-06 12:10:12 -0800

    Oprator logika

---
+++ Opratorlogika.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> logika di PHP</h1>
+
+ <?php
+
+$password = '1000';
+$password2 = '1001';
+
+if($password == $password2) {
+    echo '--------';
+    echo 'benar password tidak sama';
+    echo '--------';
+} else {
+    echo 'salah, passwordnya sama, wee';
+}
+?>
+
+<body>
+</html>
