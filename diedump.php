03342df679eba7afdde3286d444f358e2448d8d4
kristianusss <cinemaxgems@gmail.com>
2025-03-07 13:58:04 -0800

    Die dan Var dump

---
+++ diedump.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> Belajar PHP</h1>
+
+ <?php 
+
+ $car = ['mobil', 'motor'];
+ var_dump ($car);
+
+?>
+<body>
+
+</html>
