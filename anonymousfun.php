1ea778431e399bbd2b95df3015354d952680c6fd
kristianusss <cinemaxgems@gmail.com>
2025-03-07 13:19:44 -0800

    Anonymous function

---
+++ anonymousfun.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Belajar Php </title>
+</head>
+</body>
+
+    <h1> Fungsi PHP</h1>
+
+ <?php
+$a = 20000;
+$b = 30;
+function menghitung(){
+global $a, $b;
+
+$c = $a + $b;
+return $c;
+}
+//echo menghitung();
+
+$ngetrik = function($text){
+    echo $text;
+};
+
+$greeting = $ngetik;
+
+$greeting (' selamat datang semuanya 123');
+
+
+?>
+
+
+<body>
+
+</html>
